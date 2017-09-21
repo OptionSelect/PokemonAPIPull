a91324b61615f6248765c4a7ecfff02bb31a1d9b
OptionSelect <woofenwauzer@gmail.com>
2017-09-21 15:03:50 -0400

    Add project files.

---
+++ PokemonAPIPull/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace PokemonAPIPull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://pokeapi.co/api/v2/pokemon-form/58/";
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var rawResponse = String.Empty;
+
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                rawResponse = reader.ReadToEnd();
+            }
+
+            var growlithe = JsonConvert.DeserializeObject<pokemon>(rawResponse);
+
+            Console.WriteLine(growlithe.name);
+            Console.WriteLine(growlithe.id);
+            Console.WriteLine(growlithe.base_experience);
+            Console.WriteLine(growlithe.height);
+        }
+    }
+}
---
+++ PokemonAPIPull/pokemon.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonAPIPull
+{
+    class pokemon
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public string location_area_encounters { get; set; }
+        public int height { get; set; }
+        public bool is_default { get; set; }
+        public int id { get; set; }
+        public int order { get; set; }
+        public int base_experience { get; set; }
+    }
+}
